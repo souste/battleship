9b5587463ec8e0acf8704948414feba27130c1e5
Stephen <stephensouness@outlook.com>
2024-03-07 20:51:13 +0000

    webpack set up

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke() {
+  return "Why are fish so smart? Because they live in schools!";
+}
+
+export default generateJoke;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import generateJoke from "./generateJoke";
+import "./styles/main.scss";
+import glasto from "./assets/glasto.png";
+
+console.log(generateJoke());
