8f733bc254f4964c544dc19d7c7d7ff8cbd764f3
Stephen <stephensouness@outlook.com>
2024-03-12 19:42:07 +0000

    10 x 10 gameboard generated with for loop

--- src/ships.js
+++ src/ships.js
@@ -24,4 +24,31 @@ const cruiser = new Ships(3, 0, false);
 const submarine = new Ships(3, 0, false);
 const destroyer = new Ships(2, 0, false);
 
+////////////////////////////////////////////////////////////////////////////
+
+// Row and Column indexes for Gameboard are the coordinates you will use, starting at 0 and going to 9
+// In order to place a ship you will need to specify length (already done)  as well as position and direction
+// You can then use the position as the starting point for your ship and direction to know which way to fill
+// In doing so you will replace the null value at a given index !coords[x][y]! with some value to indicate a ship is there
+
+// Finally you need a method to create the ships
+// You can do this either by user input, or just by using hardcoded values in the Gameboard creator function.
+// You will also need to store all your ships in a list, and have a way to call upon the correct ship when the shot hits
+
+const board = [];
+
+for (let i = 0; i < 10; i++) {
+  const boardRow = [];
+  for (let j = 0; j < 10; j++) {
+    boardRow.push(j);
+  }
+  board.push(boardRow);
+}
+
+console.log("board", board);
+
+class Board {
+  constructor() {}
+}
+
 module.exports = Ships;
