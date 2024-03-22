a7b079b8c71e4cc69b4e483ac01bab2d77287061
Stephen <stephensouness@outlook.com>
2024-03-22 08:07:13 +0000

    Player class created but need to create instances of each of the 5 ships within the board since there is currently duplication of each ship object

--- src/board.js
+++ src/board.js
@@ -95,42 +95,43 @@ class Board {
   }
 }
 
-const carrier = new Ships(5, 0, false, "Crr");
-const battleship = new Ships(4, 0, false, "Bat");
-const cruiser = new Ships(3, 0, false, "Cru");
-const submarine = new Ships(3, 0, false, "Sub");
-const destroyer = new Ships(2, 0, false, "Des");
+// const carrier = new Ships(5, 0, false, "Crr");
+// const battleship = new Ships(4, 0, false, "Bat");
+// const cruiser = new Ships(3, 0, false, "Cru");
+// const submarine = new Ships(3, 0, false, "Sub");
+// const destroyer = new Ships(2, 0, false, "Des");
 
-playerBoard1 = new Board(carrier, battleship, cruiser, submarine, destroyer);
-playerBoard1.placeShip(carrier, 0, 0, "horizontal");
-playerBoard1.placeShip(battleship, 1, 0, "vertical");
-playerBoard1.placeShip(cruiser, 4, 3, "horizontal");
-playerBoard1.placeShip(submarine, 5, 4, "vertical");
-playerBoard1.placeShip(destroyer, 8, 6, "vertical");
+// playerBoard1 = new Board(carrier, battleship, cruiser, submarine, destroyer);
+// playerBoard1.placeShip(carrier, 0, 0, "horizontal");
+// playerBoard1.placeShip(battleship, 1, 0, "vertical");
+// playerBoard1.placeShip(cruiser, 4, 3, "horizontal");
+// playerBoard1.placeShip(submarine, 5, 4, "vertical");
+// playerBoard1.placeShip(destroyer, 8, 6, "vertical");
 
-playerBoard1.receiveAttack(0, 0);
-playerBoard1.receiveAttack(0, 1);
-playerBoard1.receiveAttack(0, 2);
-playerBoard1.receiveAttack(0, 3);
-playerBoard1.receiveAttack(0, 4);
+// // Player 2 2 will have to be randomised I assume?
 
-playerBoard1.receiveAttack(1, 0);
-playerBoard1.receiveAttack(2, 0);
-playerBoard1.receiveAttack(3, 0);
-playerBoard1.receiveAttack(4, 0);
+// playerBoard1.receiveAttack(0, 0);
+// playerBoard1.receiveAttack(0, 1);
+// playerBoard1.receiveAttack(0, 2);
+// playerBoard1.receiveAttack(0, 3);
+// playerBoard1.receiveAttack(0, 4);
 
-playerBoard1.receiveAttack(4, 2);
-playerBoard1.receiveAttack(4, 3);
-playerBoard1.receiveAttack(4, 4);
-playerBoard1.receiveAttack(4, 5);
+// playerBoard1.receiveAttack(1, 0);
+// playerBoard1.receiveAttack(2, 0);
+// playerBoard1.receiveAttack(3, 0);
+// playerBoard1.receiveAttack(4, 0);
 
-playerBoard1.receiveAttack(5, 4);
-playerBoard1.receiveAttack(6, 4);
-playerBoard1.receiveAttack(7, 4);
+// playerBoard1.receiveAttack(4, 3);
+// playerBoard1.receiveAttack(4, 4);
+// playerBoard1.receiveAttack(4, 5);
 
-playerBoard1.receiveAttack(8, 6);
-playerBoard1.receiveAttack(9, 6);
+// playerBoard1.receiveAttack(5, 4);
+// playerBoard1.receiveAttack(6, 4);
+// playerBoard1.receiveAttack(7, 4);
 
-console.log(playerBoard1);
+// playerBoard1.receiveAttack(8, 6);
+// playerBoard1.receiveAttack(9, 6);
+
+// console.log(playerBoard1);
 
 module.exports = Board;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import Ships from "./ships";
 import Board from "./board";
+import Player from "./player";
 import "./styles/main.scss";
