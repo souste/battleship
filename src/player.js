3128550f583f4fb988a4c117c45a4260c69eab09
Stephen <stephensouness@outlook.com>
2024-03-30 12:02:31 +0000

    strugging with computer random hit/miss placing and computer random ship placement, everthing changes after each new myAttack, used hard coded values for now, so that I can work on the UI and return to this later

--- src/player.js
+++ src/player.js
@@ -6,68 +6,73 @@ class Player {
   constructor() {
     this.myBoard = new Board();
     this.computerBoard = new Board();
-    this.compShipPlacement();
+    this.computerAttacks = [];
+    // this.compShipPlacement();
   }
 
-  renderMyBoard() {}
+  // renderMyBoard() {}
 
-  compShipPlacement() {
-    const flattenedBoard = this.computerBoard.board.flat();
-    const stringsToCheck = ["Crr", "Bat", "Cru", "Sub", "Des"];
-    const areAllStringsPresent = stringsToCheck.every((str) => flattenedBoard.includes(str));
+  // compShipPlacement() {
+  //   const flattenedBoard = this.computerBoard.board.flat();
+  //   const stringsToCheck = ["Crr", "Bat", "Cru", "Sub", "Des"];
+  //   const areAllStringsPresent = stringsToCheck.every((str) => flattenedBoard.includes(str));
 
-    if (!areAllStringsPresent) {
-      const ships = [
-        this.computerBoard.carrier,
-        this.computerBoard.battleship,
-        this.computerBoard.cruiser,
-        this.computerBoard.submarine,
-        this.computerBoard.destroyer,
-      ];
+  //   if (!areAllStringsPresent) {
+  //     const ships = [
+  //       this.computerBoard.carrier,
+  //       this.computerBoard.battleship,
+  //       this.computerBoard.cruiser,
+  //       this.computerBoard.submarine,
+  //       this.computerBoard.destroyer,
+  //     ];
 
-      for (const ship of ships) {
-        let placed = false;
-        while (!placed) {
-          const direction = this.randomDirection();
-          const result = this.computerBoard.placeShip(
-            ship,
-            Math.floor(Math.random() * 10),
-            Math.floor(Math.random() * 10),
-            direction
-          );
+  //     for (const ship of ships) {
+  //       let placed = false;
+  //       while (!placed) {
+  //         const direction = this.randomDirection();
+  //         const result = this.computerBoard.placeShip(
+  //           ship,
+  //           Math.floor(Math.random() * 10),
+  //           Math.floor(Math.random() * 10),
+  //           direction
+  //         );
 
-          if (result) {
-            placed = true;
-          }
-        }
-      }
-    } else {
-      return;
-    }
+  //         if (result) {
+  //           placed = true;
+  //         }
+  //       }
+  //     }
+  //   } else {
+  //     return;
+  //   }
+  // }
+
+  // randomDirection() {
+  //   const randomNumber = Math.random();
+  //   if (randomNumber < 0.5) {
+  //     return "horizontal";
+  //   } else {
+  //     return "vertical";
+  //   }
+  // }
+
+  randomCoord() {
+    return Math.floor(Math.random() * 10);
   }
 
-  randomDirection() {
-    const randomNumber = Math.random();
-    if (randomNumber < 0.5) {
-      return "horizontal";
-    } else {
-      return "vertical";
-    }
-  }
-
-  // issue to get back to but each new attack changes the ship placement state of the computer board
   myAttack(coord1, coord2) {
     let result = this.computerBoard.receiveAttack(coord1, coord2);
     if (result) {
-      this.compAttack();
+      this.compAttack(coord1, coord2);
     }
     return result;
   }
 
-  compAttack() {
-    let coord1 = Math.floor(Math.random() * 10);
-    let coord2 = Math.floor(Math.random() * 10);
-    return this.myBoard.receiveAttack(coord1, coord2);
+  compAttack(coord1, coord2) {
+    let compCoord1 = coord1 + 1;
+    let compCoord2 = coord2 + 1;
+    let result = this.myBoard.receiveAttack(compCoord1, compCoord2);
+    return result;
   }
 }
 
@@ -79,19 +84,20 @@ stephen.myBoard.placeShip(stephen.myBoard.cruiser, 4, 3, "horizontal");
 stephen.myBoard.placeShip(stephen.myBoard.submarine, 5, 4, "vertical");
 stephen.myBoard.placeShip(stephen.myBoard.destroyer, 8, 6, "vertical");
 
-renderMyBoard(stephen.myBoard.board);
-renderComputerBoard(stephen.computerBoard.board);
+stephen.computerBoard.placeShip(stephen.computerBoard.carrier, 0, 1, "horizontal");
+stephen.computerBoard.placeShip(stephen.computerBoard.battleship, 2, 5, "vertical");
+stephen.computerBoard.placeShip(stephen.computerBoard.cruiser, 4, 6, "horizontal");
+stephen.computerBoard.placeShip(stephen.computerBoard.submarine, 6, 1, "vertical");
+stephen.computerBoard.placeShip(stephen.computerBoard.destroyer, 9, 5, "vertical");
 
 stephen.myAttack(2, 5);
 stephen.myAttack(2, 6);
 stephen.myAttack(3, 3);
-// stephen.myAttack(4, 4);
-// stephen.myAttack(4, 5);
-// stephen.myAttack(4, 6);
-// stephen.myAttack(4, 8);
-// stephen.myAttack(5, 9);
-// stephen.myAttack(6, 9);
-// stephen.myAttack(7, 9);
+stephen.myAttack(4, 4);
+stephen.myAttack(4, 5);
+
+renderMyBoard(stephen.myBoard.board);
+renderComputerBoard(stephen.computerBoard.board);
 
 console.log("stephen/ my board", stephen.myBoard.board);
 console.log("stephen/ comp board", stephen.computerBoard.board);
